4de66a81c85d70dc99eaf6bb70e67500cad90bcf
Marko Melle <markmelle1@gmail.com>
2022-10-21 22:17:38 +0300

    Homerwork 2 "slider' Optimized

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,6 +1,7 @@
 const next = document.querySelector('.slider__arrow_next');
 const prev = document.querySelector('.slider__arrow_prev');
 let itemsArr = Array.from(document.querySelectorAll('.slider__item'));
+const dot = Array.from(document.querySelectorAll('.slider__dot'));
 
 let getcurrentNumber = function () {
    return itemsArr.findIndex(element => element.className.includes('slider__item_active'));
@@ -11,53 +12,52 @@ const prevNumber = () => {
    if (currentNumber <= 0) {
       return (itemsArr.length - 1);
    }
-   return (currentNumber - 1);
+   return currentNumber - 1;
 }
 
 const nextNumber = () => {
    if (currentNumber >= (itemsArr.length - 1)) {
       return 0;
    }
-   return (currentNumber + 1);
+   return currentNumber + 1;
 }
+
+function slideIt(ArrayOfElements, className, nextNumber, current = currentNumber) {
+   ArrayOfElements[current].classList.remove(className);
+   ArrayOfElements[nextNumber].classList.add(className);
+};
+
+function counterReload() {
+   currentNumber = getcurrentNumber()
+   currentDot = getcurrentDot()
+};
 
 prev.onclick = function () {
-   itemsArr[currentNumber].classList.remove('slider__item_active');
-   itemsArr[prevNumber()].classList.add('slider__item_active');
-   dot[currentNumber].classList.remove('slider__dot_active');
-   dot[prevNumber()].classList.add('slider__dot_active');
-   currentNumber = getcurrentNumber();
-   currentDot = getcurrentDot()
-}
+   slideIt(itemsArr, 'slider__item_active', prevNumber());
+   slideIt(dot, 'slider__dot_active', prevNumber());
+   counterReload();
+};
 
 next.onclick = function () {
-   itemsArr[currentNumber].classList.remove('slider__item_active');
-   itemsArr[nextNumber()].classList.add('slider__item_active');
-   dot[currentNumber].classList.remove('slider__dot_active');
-   dot[nextNumber()].classList.add('slider__dot_active');
-   currentNumber = getcurrentNumber();
-   currentDot = getcurrentDot()
-}
+   slideIt(itemsArr, 'slider__item_active', nextNumber());
+   slideIt(dot, 'slider__dot_active', nextNumber());
+   counterReload();
+};
 
 // Точки начало
-
-const dot = Array.from(document.querySelectorAll('.slider__dot'));
 
 dot[currentNumber].classList.add('slider__dot_active');
 
 let getcurrentDot = function () {
    return dot.findIndex(element => element.className.includes('slider__dot_active'));
-}
+};
 
 let currentDot = getcurrentDot();
 
 for (let i = 0; i < dot.length; i++) {
    dot[i].onclick = () => {
-      dot[currentDot].classList.remove('slider__dot_active');
-      dot[i].classList.add('slider__dot_active');
-      itemsArr[getcurrentNumber()].classList.remove('slider__item_active');
-      itemsArr[i].classList.add('slider__item_active');
-      currentDot = getcurrentDot();
-      currentNumber = getcurrentNumber();
+      slideIt(dot, 'slider__dot_active', i, currentDot);
+      slideIt(itemsArr, 'slider__item_active', i);
+      counterReload();
    }
-}
+};
