a7d1a7351c3f717e8669f8c6b75434e70ec00a2a
Marko Melle <markmelle1@gmail.com>
2022-10-21 18:23:53 +0300

    Homework 1/Version 2 added

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,27 +1,27 @@
 const clickCounter = document.getElementById('clicker__counter');
 const btn = document.getElementById('cookie');
 const speedField = document.getElementById('clicker__speed');
+const cookie = document.getElementById('cookie');
 
 let firstClick = true;
-let counter = 0;
 let currentTime;
 let lastTime;
+
+clickCounter.textContent = 0;
 
 btn.onclick = function () {
    if (firstClick) {
       lastTime = new Date();
-      console.log(counter);
-      counter += 1;
-      clickCounter.textContent = counter;
+      ++clickCounter.textContent;
       firstClick = false
+      cookie.width = clickCounter.textContent % 2 ? 250 : 200;
       return;
    };
    currentTime = new Date();
-   let resultTime = currentTime - lastTime;
+   const resultTime = currentTime - lastTime;
    speedField.textContent = 1 / (resultTime * 0.001);
    speedField.textContent = speedField.textContent.substring(0, 4);
    lastTime = new Date();
-   console.log(counter);
-   counter += 1;
-   clickCounter.textContent = counter;
-}
+   ++clickCounter.textContent;
+   cookie.width = clickCounter.textContent % 2 ? 250 : 200;
+};
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,17 +4,23 @@ const reset = document.getElementById('reset-game');
 
 const hole = function (index) {
    return document.getElementById(`hole${index}`)
-}
+};
 
 for (let i = 1; i < 10; i++) {
    hole(i).onclick = function () {
       if (hole(i).className.includes('hole_has-mole')) {
-         console.log(dead);
          ++dead.textContent;
+         if (+dead.textContent === 10) {
+            alert('Вы выиграли');
+            lost.textContent = dead.textContent = 0;
+         }
       } else {
-         console.log(lost);
          ++lost.textContent;
-      }
-   }
-}
+         if (+lost.textContent === 5) {
+            alert('Вы проиграли');
+            lost.textContent = dead.textContent = 0;
+         };
+      };
+   };
+};
 
