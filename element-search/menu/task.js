e474eb7d408efe727e09017d5e9c2bbb53c45592
Marko Melle <markmelle1@gmail.com>
2022-10-24 12:12:03 +0300

    Homework 2/Version 3 added

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,37 @@
 const menuLink = Array.from(document.querySelectorAll('.menu__link'));
 const menuSub = Array.from(document.querySelectorAll('.menu_sub'));
+let menuIsClosed = true;
+
+function menuClose() {
+   menuSub.forEach(
+      (subMenu) => {
+         subMenu.classList.remove('menu_active');
+         menuIsClosed = true;
+      });
+};
+
+function menuOpen(link) {
+   link.parentElement.querySelector('.menu_sub').classList.add('menu_active');
+   menuIsClosed = false;
+};
 
 menuLink.forEach(link => {
    link.onclick = () => {
-      menuSub.forEach(
-         (subMenu, index) => {
-            if (link.parentElement.querySelector('.menu_active')) {
-               subMenu[index].classList.remove('menu_active');
-            }
-            subMenu.classList.remove('menu_active')
-         });
-      if (link.parentElement.querySelector('.menu_sub')) {
-         link.parentElement.querySelector('.menu_sub').classList.add('menu_active');
+      if (link.parentElement.querySelector('.menu_active') && !menuIsClosed) {
+         menuClose();
          return false;
-      }
-   }
-})
+      };
+
+      if (!menuIsClosed && link.parentElement.querySelector('.menu_sub')) {
+         menuClose();
+         menuOpen(link);
+         return false;
+      };
+
+      if (link.parentElement.querySelector('.menu_sub')) {
+         menuOpen(link);
+         return false;
+      };
+   };
+});
 
