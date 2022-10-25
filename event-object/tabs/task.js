3195fcfd477ef128d8a0974f137433c234754a5b
Marko Melle <markmelle1@gmail.com>
2022-10-25 14:49:37 +0300

    Homework 2/Tabs Changed

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,19 +1,40 @@
+//Делегирование
+
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
-let activeTab;
 
 function getActiveTab() {
-   activeTab = tabs.indexOf(document.querySelector('.tab_active'));
+   return tabs.indexOf(document.querySelector('.tab_active'));
 }
 
-function tabSwitch(clickedTab) {
-   getActiveTab();
-   tabs[activeTab].classList.remove('tab_active');
-   tabsContent[activeTab].classList.remove('tab__content_active');
-   tabs[clickedTab].classList.add('tab_active');
-   tabsContent[clickedTab].classList.add('tab__content_active');
+function tabToggle(Event) {
+   if (Event.target.classList.contains('tab')) {
+      tabs.forEach(elm => elm.classList.remove('tab_active'))
+      Event.target.classList.add('tab_active');
+      tabsContent.forEach(elm => elm.classList.remove('tab__content_active'))
+      tabsContent[getActiveTab()].classList.add('tab__content_active');
+   } else return;
 };
 
-tabs.forEach(function (Element, index) {
-   Element.addEventListener('click', () => tabSwitch(index));
-});
+window.tabs1.addEventListener('click', tabToggle); //Делегирование
+
+
+// Обработчик на каждый элемент
+
+// const tabs = Array.from(document.querySelectorAll('.tab'));
+// const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
+
+// function getActiveTab() {
+//    return tabs.indexOf(document.querySelector('.tab_active'));
+// }
+
+// function tabSwitch(clickedTab) {
+//    tabs[getActiveTab()].classList.remove('tab_active');
+//    tabsContent[getActiveTab()].classList.remove('tab__content_active');
+//    tabs[clickedTab].classList.add('tab_active');
+//    tabsContent[clickedTab].classList.add('tab__content_active');
+// };
+
+// tabs.forEach(function (Element, index) {
+//    Element.addEventListener('click', () => tabSwitch(index));
+// });
